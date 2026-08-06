fa5fd5fa59d8ede648861653fb44ad75eb38a36f
sachin <sachinnarofficials@gmail.com>
2026-08-06 14:13:55 +0530

    delete transaction

--- project/wallet-full-stack-app/backend/server.js
+++ project/wallet-full-stack-app/backend/server.js
@@ -8,6 +8,8 @@ const app = express();
 
 const PORT = process.env.PORT || 5001;
 
+app.use(express.json());
+
 async function initDB() {
   try {
     await sql`CREATE TABLE IF NOT EXISTS transactions (
@@ -24,19 +26,63 @@ async function initDB() {
   }
 }
 
-// app.post("/api/transactions", async (req, res) => {
-//   try {
-//     const { title, amount, category, user_id } = req.body;
-//     if (!title || !amount || !category || !user_id) {
-//       return res.status(400).json({ error: "Missing required fields" });
-//     }
-//   } catch (error) {}
-// });
+app.get("/", (req, res, next) => {
+  console.log("Hey we hit a req, the method is", req.method);
+  next();
+});
 
-// app.get("/", (req, res, next) => {
-//   console.log("Hey we hit a req, the method is", req.method);
-//   next();
-// });
+app.get(`/api/transactions/:userId`, async (req, res) => {
+  try {
+    const { userId } = req.params;
+    console.log("userId", userId);
+    const transactions = await sql`
+    SELECT * FROM transactions WHERE user_id = ${userId} ORDER BY created_at DESC;`;
+
+    res.status(200).json(transactions);
+  } catch (error) {
+    console.log("Error fetching the transaction", error);
+    res.status(500).json({ message: "Internal server error" });
+  }
+});
+
+app.delete("/api/transactions/:id", async (req, res) => {
+  try {
+    const { id } = req.params;
+
+    const result = await sql`
+    DELETE FROM transactions WHERE id = ${id} RETURNING *`;
+
+    if (result.length === 0) {
+      return res.status(404).json({ message: "Transaction not found" });
+    }
+    res.status(200).json({ message: "Transaction deleted successfully" });
+  } catch (error) {
+    console.log("Error deleting the transaction", error);
+    res.status(500).json({ message: "Internal server error" });
+  }
+});
+
+app.post("/api/transactions", async (req, res) => {
+  try {
+    const { title, amount, category, user_id } = req.body;
+
+    if (!title || !amount || !category || !user_id) {
+      return res.status(400).json({ error: "Missing required fields" });
+    }
+
+    const transaction = await sql`
+    INSERT INTO transactions (user_id, title, amount, category)
+    VALUES (${user_id}, ${title}, ${amount}, ${category})
+    RETURNING *;
+    `;
+
+    console.log("transaction", transaction);
+    res.status(201).json(transaction);
+  } catch (error) {
+    console.log("Error creating the transaction", error);
+    res.status(500).json({ message: "Internal server error" });
+  }
+});
 
 initDB().then(() => {
   app.listen(PORT, () => {
